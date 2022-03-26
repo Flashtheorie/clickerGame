ef163e35d114ac2d9e25565a9045910c749a41be
Antoine <antoinepascualmail@gmail.com>
2022-03-26 11:30:28 +0100

    Ajout Jquery

--- index.php
+++ index.php
@@ -4,8 +4,21 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Accueil</title>
+	<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-	
+	<div class="container" align="center">
+	<h1>test</h1>
+	<img src="img/dollar.png" alt="">
+
+
+
+
+
+	</div>
+
+
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 </body>
 </html>
